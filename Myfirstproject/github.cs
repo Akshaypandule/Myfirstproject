868f4675d0758b712568f3aa07c273ec2d11c6c1
Akshay Pandule <akshaypandule7172@gmail.com>
2022-08-09 23:17:44 +0530

    new

---
+++ Myfirstproject/github.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Myfirstproject
+{
+    class github
+    {
+		public abstract class A
+		{
+			public abstract void msg();
+		}
+
+		public class B : A
+		{
+			public override void msg()
+			{
+				Console.WriteLine("msg is implemented here");
+			}
+
+			public static void Main(string[] args)
+			{
+				A t1 = new B();
+				t1.msg();
+			}
+		}
+	}
+}
