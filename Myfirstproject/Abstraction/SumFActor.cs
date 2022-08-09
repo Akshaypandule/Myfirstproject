c74ee7eea43d3cd04fb5fb70fb83960c87fb1afd
Akshay Pandule <akshaypandule7172@gmail.com>
2022-08-10 00:16:48 +0530

    nxt

---
+++ Myfirstproject/Abstraction/SumFActor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Myfirstproject.Abstraction
+{
+    interface Ifacable
+    {
+        public void Display(int a);
+    }
+    class SumFActor : Ifacable
+    {
+        public void Display(int a)
+        {
+            int sum = 0;
+            for(int i=1;i<=a;i++)
+            {
+                if(a%i==0)
+                {
+                    sum = sum + i;
+                }
+            }
+            Console.WriteLine("Sum of factor is="  + sum);
+
+        }
+    }
+    class factor
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            SumFActor b = new SumFActor();
+            b.Display(a);
+        }
+    }
+        
+    
+}
---
+++ Myfirstproject/Interview Question/ABC.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Myfirstproject.Interview_Question
+{  // Palindrome number 
+    class ABC
+    {
+        public static void Main(string[] args)
+        {
+            int n, num, r, rev = 0;
+
+            Console.Write("Enter a number:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            num = n;
+
+            while (n > 0)
+            {
+                r = n % 10;
+                rev = rev * 10 + r;
+                n = n / 10;
+            }
+
+            if (num == rev)
+            {
+                Console.WriteLine("Number is palindrome:" + num);
+            }
+            else
+            {
+                Console.WriteLine("Number is not palindrome:" + num);
+            }
+        }
+    }
+}  
